aa220b360da1576d4470ef336cd48e60fdc0e298
ylyu4 <ylyu4@gwu.edu>
2023-05-25 16:43:03 +0800

    refactor code

--- src/main/java/org/example/DistanceCalculator.java
+++ src/main/java/org/example/DistanceCalculator.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class DistanceCalculator {
 
-    public static final String graph = "AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7";
+    public static final String GRAPH = "AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7";
 
     public static Integer calculate(String... args) {
-        String start = args[0];
-        String end = args[1];
-        String[] splitGraph = graph.split(",");
-        String combination = start + end;
-        String result = Arrays.stream(splitGraph).filter(it -> it.startsWith(combination)).findFirst().get();
-        return Integer.parseInt(result.substring(result.length() - 1));
+        String[] splitGraph = GRAPH.split(",");
+        String combination = args[0] + args[1];
+        return Integer.parseInt(Arrays.stream(splitGraph)
+                .filter(it -> it.startsWith(combination))
+                .findFirst()
+                .map(it -> it.substring(it.length() - 1))
+                .get());
     }
 }
